70f068f755795276aeaefb911a76c7204c396d09
Juan Fernando Restrepo <JuanRestrepo>
2020-07-29 19:17:05 -0500

    Se refactoriza codigo creando archivo de rutas propio para usuarios y referenciandolo en el routes general de la aplicacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,14 @@
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
-import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
-import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
-import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
+import { USUARIO_ROUTES } from './components/usuario/usuario.routes';
 
 const APP_ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
     { 
         path: 'usuario/:id',
         component: UsuarioComponent,
-        children: [
-            { path: 'nuevo', component: UsuarioNuevoComponent },
-            { path: 'detalle', component: UsuarioDetalleComponent },
-            { path: 'editar', component: UsuarioEditarComponent },
-            { path: '**', pathMatch: 'full', redirectTo: 'nuevo' }
-        ]
+        children: USUARIO_ROUTES
     },
     { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
