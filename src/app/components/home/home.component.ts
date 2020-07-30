8d95e892740d6ac4f59017d78f791a6bd4de470a
Juan Fernando Restrepo <JuanRestrepo>
2020-07-29 19:39:00 -0500

    Se agrega implementacion de metodos para el ciclo de vida de la aplicacion angular que permite comprender como funcionan

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,26 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   template: `
      <app-ng-style></app-ng-style>
+     <br>
+
      <app-css></app-css>
+     <br>
      <app-clases></app-clases>
+     <br><br>
 
      <p [appResaltado]="'red'">
        Hola Mundo
      </p>
 
+     <br>
      <app-ng-swich></app-ng-swich>
   `,
   styles: [
   ]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent 
+implements 
+OnInit, 
+OnChanges,
+DoCheck,
+AfterContentInit,
+AfterContentChecked,
+AfterViewInit,
+AfterViewChecked,
+OnDestroy {
 
-  constructor() { }
+  constructor() {
+    console.log('constructor'); 
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
+  ngOnChanges(): void {
+    console.log('ngOnChanges');
+  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
   }
 
 }
