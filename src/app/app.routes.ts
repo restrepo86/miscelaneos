6ed0dd6cce76dc09ad896c25487082dcdaeca444
Juan Fernando Restrepo <JuanRestrepo>
2020-07-29 19:08:58 -0500

    Se crea implementacion para trabajar con rutas hijas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,23 @@
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
+import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
+import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
+import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
 
 const APP_ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: 'usuario', component: UsuarioComponent },
-    { path: '', pathMatch: 'full', redirectTo: 'home' }
+    { 
+        path: 'usuario/:id',
+        component: UsuarioComponent,
+        children: [
+            { path: 'nuevo', component: UsuarioNuevoComponent },
+            { path: 'detalle', component: UsuarioDetalleComponent },
+            { path: 'editar', component: UsuarioEditarComponent },
+            { path: '**', pathMatch: 'full', redirectTo: 'nuevo' }
+        ]
+    },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
